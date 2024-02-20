0351e2b79b02f5c19be78ecb22d91eedf5bc76e4
thalles-victor <thalles.victor2012@gmail.com>
2024-02-20 16:35:17 -0300

    :sparkles: feat: create file to export components from a single location.

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { CustomButton } from "./CustomButton";
+import { Hero } from "./Hero";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
+
+export { CustomButton, Hero, Navbar, Footer };
